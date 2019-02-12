a9af260ff72e2de254be4320166b0e3d71196708
Jurandir dos Santos <c070622@corp.caixa.gov.br>
2019-02-12 11:46:42 -0200

    insert subtotal

--- c02/js/example.js
+++ c02/js/example.js
@@ -4,5 +4,14 @@ var message = ', please check your order.';
 var elWelcome = document.getElementById('welcome');
 elWelcome.textContent = greeting + name + message;
 
-var sign = 'Montage House';
+var sign = 'Montague House';
 var tiles = sign.length;
+var price = 5;
+var cost = price * tiles;
+
+var elSign = document.getElementById('sign');
+var elTiles = document.getElementById('tiles');
+var elCost = document.getElementById('subtotal');
+elSign.textContent = sign;
+elTiles.textContent = tiles;
+elCost.textContent = cost;
