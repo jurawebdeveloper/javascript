f3f959111170f02ef5d44afa3e0230442323649f
Jurandir dos Santos <c070622@corp.caixa.gov.br>
2019-02-12 12:20:47 -0200

    shipping

--- c02/js/example.js
+++ c02/js/example.js
@@ -8,10 +8,16 @@ var sign = 'Montague House';
 var tiles = sign.length;
 var price = 5;
 var cost = price * tiles;
+var shipping = 7;
+var total = shipping + cost;
 
 var elSign = document.getElementById('sign');
 var elTiles = document.getElementById('tiles');
 var elCost = document.getElementById('subtotal');
+var elShipping = document.getElementById('shipping');
+var elTotal = document.getElementById('total');
 elSign.textContent = sign;
 elTiles.textContent = tiles;
 elCost.textContent = cost;
+elShipping.textContent = shipping;
+elTotal.textContent = total;
