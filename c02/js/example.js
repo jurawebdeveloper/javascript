e2a0cd83ae477cf677a5f836b1b4da8def98d6b9
Jurandir dos Santos <c070622@corp.caixa.gov.br>
2019-02-12 11:34:44 -0200

    message

---
+++ c02/js/example.js
@@ -0,0 +1,8 @@
+var name = 'Molly';
+var greeting = 'Hello ';
+var message = ', please check your order.';
+var elWelcome = document.getElementById('welcome');
+elWelcome.textContent = greeting + name + message;
+
+var sign = 'Montage House';
+var tiles = sign.length;
