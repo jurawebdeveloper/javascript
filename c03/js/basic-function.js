99bdc8abf9bd551f80cdad06e7091cb6ede7d512
Jurandir <jura.webdeveloper@gmail.com>
2019-02-13 11:14:40 -0200

    incluindo ma funcao js

--- c03/js/basic-function.js
+++ c03/js/basic-function.js
@@ -1,1 +1,6 @@
 var msg = 'Cadastre-se para receber nossas newsletter com 10% de desconto!';
+function updateMessage() {
+	var el = document.getElementById('message');
+	el.textContent = msg;
+}
+updateMessage();
