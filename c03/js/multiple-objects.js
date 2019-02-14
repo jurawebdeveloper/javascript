7919c3f2b84ce9118b3161ce18186f42e028a810
Jura <jura.webdeveloper@gmail.com>
2019-02-14 10:43:50 -0200

    multiplos objetos final

--- c03/js/multiple-objects.js
+++ c03/js/multiple-objects.js
@@ -14,6 +14,11 @@ var tullipHotel = new Hotel('Tullip In', 80,64);
 var elName1 = document.getElementById('hotelName');
 elName1.textContent = ibisHotel.name;
 
+var elRoomsAv1 = document.getElementById('rooms');
+elRoomsAv1.textContent = ibisHotel.checkAvailability();
 
 var elName2 = document.getElementById('hotelName2');
 elName2.textContent = tullipHotel.name;
+
+var elRoomsAv2 = document.getElementById('rooms2');
+elRoomsAv2.textContent = tullipHotel.checkAvailability();
