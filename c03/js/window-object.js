ea8346b962100feb12b9bdd2240745c836e93c5f
Jura <jura.webdeveloper@gmail.com>
2019-02-15 13:28:22 -0200

    document object model final

--- c03/js/window-object.js
+++ c03/js/window-object.js
@@ -1,5 +1,8 @@
 var msg = '<h2>Browser Window</h2><p>width: ' + window.innerWidth + '</p>';
 	msg += '<p>height: ' + window.innerHeight + '</p>'
-	msg += '<h2>History</h2>'
+	msg += '<h2>History</h2><p>itens: ' + window.history.length + '</p>';
+	msg += '<h2>Screen</h2><p>width: ' + window.screen.width + '</p>';
+	msg += '<p>heigth: ' + window.screen.height + '</p>'
 var elMsg = document.getElementById('info');
 elMsg.innerHTML = msg;
+alert('Página atual: '+ window.location);
