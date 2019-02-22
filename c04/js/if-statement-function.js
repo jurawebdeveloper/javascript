572528322b7a2d26a1b7bc1675c28148e7a686db
Jura <jura.webdeveloper@gmail.com>
2019-02-22 09:54:43 -0300

    final if funcao

--- c04/js/if-statement-function.js
+++ c04/js/if-statement-function.js
@@ -1,5 +1,13 @@
 var score = 75;
-var msg = 'js funcionou!';
+var msg = 'js funcionou! ';
 
+function congratulate(){
+	msg += 'Parabéns! ';
+}
+
+if (score >= 50) {
+	congratulate();
+	msg += 'Siga para a próxima etapa.'
+}
 var elMsg = document.getElementById('resposta');
 elMsg.textContent = msg
