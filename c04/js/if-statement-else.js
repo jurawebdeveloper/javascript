c5b0d69ab3fffd1a7c9e232530e55a40912b6e51
Jura <jura.webdeveloper@gmail.com>
2019-02-22 10:05:10 -0300

    if else final

--- c04/js/if-statement-else.js
+++ c04/js/if-statement-else.js
@@ -1,3 +1,11 @@
-var msg = 'js funcionou!';
+var msg = 'js funcionou! ';
+var score = 45;
+var pass = 50;
+
+if(score >= pass){
+	msg += 'Parabéns, você passou!';
+} else {
+	msg += 'Sorry, baby, tente novamente';
+}
 var elMsg = document.getElementById('resposta');
 elMsg.textContent = msg;
