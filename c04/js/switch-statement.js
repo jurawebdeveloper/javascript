208fa302f8217e721a5bf1a8cecd776acefaa105
Jura <jura.webdeveloper@gmail.com>
2019-02-22 10:24:38 -0300

    final instrucao switch

--- c04/js/switch-statement.js
+++ c04/js/switch-statement.js
@@ -1,13 +1,19 @@
 var msg = 'js funcionou! ';
-var level = 1;
+var level = 3;
 
 switch (level){
 	case 1:
 		msg += 'Boa sorte no pripeiro teste!';
-	break;
+		break;
 	case 2:
 		msg += 'Segundo de 3 níveis - siga em frente!';
-	break;
+		break;
+	case 3:
+		msg += 'Etapa final, quase lá!';
+		break;
+	default:
+		msg += 'Boa sorte';
+		break;
 }
 
 var elMsg = document.getElementById('resposta');
