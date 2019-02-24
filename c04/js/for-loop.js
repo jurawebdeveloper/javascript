2ec9376f7d482fcb7f7a0db05d9c8b79f0b47cdb
Jurandir <jura.webdeveloper@gmail.com>
2019-02-24 08:54:26 -0300

    testando o loop for

--- c04/js/for-loop.js
+++ c04/js/for-loop.js
@@ -1,9 +1,9 @@
-var scores = [24,32,17];
+var scores = [24,32,17,42,74];
 var arrayLength = scores.length;
 var roundNumber = 0;
 var i;
 
-var msg = scores.length;
+var msg = 'Nº de rounds: ' + scores.length + '<br />';
 
 for (i=0; i < arrayLength; i++) {
     roundNumber = (1+i);
