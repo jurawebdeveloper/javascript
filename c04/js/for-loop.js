3e288b83211a1079d41d14ef422a328b916fd397
Jurandir <jura.webdeveloper@gmail.com>
2019-02-24 08:51:00 -0300

    loop for funcionando!

--- c04/js/for-loop.js
+++ c04/js/for-loop.js
@@ -3,11 +3,13 @@ var arrayLength = scores.length;
 var roundNumber = 0;
 var i;
 
-var msg = 'js works';
-for (i=0; i<arrayLength;) {
+var msg = scores.length;
+
+for (i=0; i < arrayLength; i++) {
     roundNumber = (1+i);
     msg += 'Round ' + roundNumber + ': ';
     msg += scores[i] + '<br />';
+    //i = i + 1;
 }
 
 var elMsg = document.getElementById('resposta');
