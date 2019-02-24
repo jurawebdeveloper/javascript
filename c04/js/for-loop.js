cc6b159dd93501804b09934c122da7e6d57715d1
Jurandir <jura.webdeveloper@gmail.com>
2019-02-24 08:43:57 -0300

    usando o for

--- c04/js/for-loop.js
+++ c04/js/for-loop.js
@@ -1,3 +1,14 @@
+var scores = [24,32,17];
+var arrayLength = scores.length;
+var roundNumber = 0;
+var i;
+
 var msg = 'js works';
+for (i=0; i<arrayLength;) {
+    roundNumber = (1+i);
+    msg += 'Round ' + roundNumber + ': ';
+    msg += scores[i] + '<br />';
+}
+
 var elMsg = document.getElementById('resposta');
 elMsg.innerHTML = msg;
