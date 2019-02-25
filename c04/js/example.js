cbd576056394abc3906d4c0f2d17bab48a0a3144
Jura <jura.webdeveloper@gmail.com>
2019-02-25 10:20:17 -0300

    exemplo finalizado

--- c04/js/example.js
+++ c04/js/example.js
@@ -1,2 +1,18 @@
 
-document.getElementById('blackboard').innerHTML = 'tabuada';
+var table = 4;
+var operador = 'multiplicacao';
+var i = 1;
+var msg = 'tabuada <br />';
+
+if (operador === 'adicao') {
+	while (i < 11) {
+		msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
+		i++;
+	}
+} else {
+	while (i < 11) {
+		msg += i + ' x ' + table + ' = ' + (i*table) + '<br />';
+		i++;
+	}
+}
+document.getElementById('blackboard').innerHTML = msg;
