f5b3b7c32b26eadcbb2cfc6cccfed79fbd37aea9
Jura <jura.webdeveloper@gmail.com>
2019-02-25 09:43:11 -0300

    while - final

--- c04/js/while-loop.js
+++ c04/js/while-loop.js
@@ -1,1 +1,8 @@
-document.write('Js funciona');
+var i = 1;
+var msg = '';
+while (i < 10){
+	msg += i + 'x 5 = ' + (i*5) + '<br />';
+	i = i + 1;
+}
+
+document.getElementById('resposta').innerHTML = msg;
