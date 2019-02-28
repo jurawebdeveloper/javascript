3113212a4f4a9520b7c2ae5cfb61caec355683b0
Jura <jura.webdeveloper@gmail.com>
2019-02-28 16:32:17 -0300

    sleep nao funcionou!

--- c05/js/node-list.js
+++ c05/js/node-list.js
@@ -1,6 +1,15 @@
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+async function breaker() {
+	await sleep(5000);
+}
+
+
 var hotItens = document.querySelectorAll('li.hot');
 if (hotItens.length > 0) {
 	for (var i = 0; i < hotItens.length; i++) {
 		hotItens[i].className = 'cool';
+		breaker();
 	}
 }
