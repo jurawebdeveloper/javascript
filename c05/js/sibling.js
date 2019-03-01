cbc881db7e94e7babdaf5c8faeaa5b912981cfd3
Jura <jura.webdeveloper@gmail.com>
2019-03-01 11:09:22 -0300

    previous nao funciona

--- c05/js/sibling.js
+++ c05/js/sibling.js
@@ -1,1 +1,6 @@
-var start
+var startItem = document.getElementById('two');
+var prevItem = startItem.previousSibling;
+var nextItem = startItem.nextSibling;
+
+//nextItem.className = 'complete';
+startItem.className = 'complete';
