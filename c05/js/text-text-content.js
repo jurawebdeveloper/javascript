8a43ec04dbb4d26934ecae6ec740b34e412ab930
Jurandir <jura.webdeveloper@gmail.com>
2019-03-03 21:03:45 -0300

    text content funcionando

--- c05/js/text-text-content.js
+++ c05/js/text-text-content.js
@@ -1,2 +1,10 @@
 var firstItem = document.getElementById('one');
-firstItem.className = 'cool';
+var showTextContent = firstItem.textContent;
+var showInnerText = firstItem.innerText;
+
+var msg = '<p>textContent: ' + showTextContent + '</p>';
+    msg += '<p>innerText: ' + showInnerText + '</p>';
+var el = document.getElementById('resultado');
+el.innerHTML = msg;
+firstItem.textContent = 'pão';
+//firstItem.className = 'cool';
