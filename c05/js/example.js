0efaaa497598bb6ed45ad753b3fefdbd6f5fdc7d
Jura <jura.webdeveloper@gmail.com>
2019-03-07 08:47:27 -0300

    teste script for

--- c05/js/example.js
+++ c05/js/example.js
@@ -1,12 +1,20 @@
-var list = document.getElementsByTagName('ul')[0];
+var listLi = document.getElementsByTagName('ul')[0];
 
 var newItemLast = document.createElement('li');
 var newTextLast = document.createTextNode('banana');
 
 newItemLast.appendChild(newTextLast);
-list.appendChild(newItemLast);
+listLi.appendChild(newItemLast);
 
 var newItemFirst = document.createElement('li');
 var newTextFirst = document.createTextNode('ice tea');
 newItemFirst.appendChild(newTextFirst);
-list.insertBefore(newItemFirst, list.firstChild);
+listLi.insertBefore(newItemFirst, listLi.firstChild);
+
+listLi2 = document.querySelectorAll('li');
+var el = document.getElementById('one')
+el.innerHTML = 'apple';
+var i;
+for (i = 0; i < listLi2.length; i++) {
+	listLi2[i].className = 'cool';
+}
