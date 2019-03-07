216066b3405fcd18b2e1282f0390bd5152b3c13a
Jura <jura.webdeveloper@gmail.com>
2019-03-07 08:57:05 -0300

    script finalizado

--- c05/js/example.js
+++ c05/js/example.js
@@ -12,9 +12,14 @@ newItemFirst.appendChild(newTextFirst);
 listLi.insertBefore(newItemFirst, listLi.firstChild);
 
 listLi2 = document.querySelectorAll('li');
-var el = document.getElementById('one')
-el.innerHTML = 'apple';
 var i;
 for (i = 0; i < listLi2.length; i++) {
 	listLi2[i].className = 'cool';
 }
+
+
+var heading = document.querySelector('h2');
+var headingText = heading.firstChild.nodeValue;
+var totalItems = listLi2.length;
+var newHeading = headingText + '<span>' + totalItems + '</span>';
+heading.innerHTML = newHeading;
