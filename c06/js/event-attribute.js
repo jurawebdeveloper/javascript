3e79a410b74201742b4b8219a6506bdb2599a279
Jura <jura.webdeveloper@gmail.com>
2019-03-08 10:57:47 -0300

    script funcionando

--- c06/js/event-attribute.js
+++ c06/js/event-attribute.js
@@ -1,1 +1,11 @@
-document.write("<h2>testando script...</h2>");
+//document.write("<h2>testando script...</h2>");
+function checkUsername() {
+	var elMsg = document.getElementById('feedback');
+	var elUsername = document.getElementById('username');
+	
+	if (elUsername.value.length < 5) {
+		elMsg.textContent = 'O nome deve ter pelo menos 5 caracteres.';
+	} else {
+		elMsg.textContent = '';
+	}
+}
