4d4ad58d1754c71d991c2a9a968a711c9af2b2cb
Jura <jura.webdeveloper@gmail.com>
2019-03-08 11:38:44 -0300

    script funcionando

--- c06/js/event-handler.js
+++ c06/js/event-handler.js
@@ -1,9 +1,12 @@
-document.write('<h2>Testando script...</h2>')
+//document.write('<h2>Testando script...</h2>')
 function checkUsername() {
 	var elMsg = document.getElementById('feedback');
 	if (this.value.length < 5) {
-		
+		elMsg.textContent = 'Nome deve ter no mínimo 5 caracteres.';
+	} else {
+		elMsg.textContent = '';
 	}	
 }
 
 var elUsername = document.getElementById('username');
+elUsername.onblur = checkUsername;
