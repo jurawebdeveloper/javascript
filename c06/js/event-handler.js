aa74b5871d05c6ed4d2a8a1a57a6ba2d0fbe7729
Jura <jura.webdeveloper@gmail.com>
2019-03-08 11:33:41 -0300

    testando script handler event

--- c06/js/event-handler.js
+++ c06/js/event-handler.js
@@ -1,1 +1,9 @@
-function checkUsername() {}
+document.write('<h2>Testando script...</h2>')
+function checkUsername() {
+	var elMsg = document.getElementById('feedback');
+	if (this.value.length < 5) {
+		
+	}	
+}
+
+var elUsername = document.getElementById('username');
