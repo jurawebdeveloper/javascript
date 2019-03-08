8c0b67a6ca0ca9e687cd12cd7a00ebfd3891362c
Jura <jura.webdeveloper@gmail.com>
2019-03-08 12:37:30 -0300

    script funcionando

--- c06/js/event-listener.js
+++ c06/js/event-listener.js
@@ -1,1 +1,12 @@
-document.write('<h2>testando arquivo js...</h2>');
+//document.write('<h2>testando arquivo js...</h2>');
+function checkUsername() {
+	var elMsg = document.getElementById('feedback');
+	if(this.value.length < 5) {
+		elMsg.textContent = 'O nome deve conter no mínimo 5 caracteres.';
+	} else {
+		elMsg.textContent = '';
+	}
+}
+
+var elUsername = document.getElementById('username');
+elUsername.addEventListener('blur', checkUsername, false);
