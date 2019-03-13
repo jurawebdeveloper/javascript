97ec6da635bbd3ee3967fe13268866346cf5414b
Jura <jura.webdeveloper@gmail.com>
2019-03-13 12:48:15 -0300

    script funcionando - sem verificar se é IE 8 ou mais antigo

--- c06/js/event-delegation.js
+++ c06/js/event-delegation.js
@@ -1,1 +1,16 @@
-document.write('<h2>Teste...</h2>')
+//document.write('<h2>Teste...</h2>')
+function itemDone(e) {
+	var target, elParent, elGrandParent, elMsg;
+	elMsg = document.getElementById('mensagem');
+	target = e.target;
+	elParent = target.parentNode;
+	elGrandParent = target.parentNode.parentNode;
+	//elMsg.innerHTML = target.textContent;
+	elGrandParent.removeChild(elParent);
+	e.preventDefoult();
+	
+}
+
+var el = document.getElementById('listaCompras');
+
+el.addEventListener('click', function(e){itemDone(e)}, 'false');
