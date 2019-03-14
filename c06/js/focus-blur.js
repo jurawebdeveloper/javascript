f65cde9adb2ca77b3debe1298e22d29ecd42e7d1
Jura <jura.webdeveloper@gmail.com>
2019-03-14 10:19:24 -0300

    funcao checklength funcionando

--- c06/js/focus-blur.js
+++ c06/js/focus-blur.js
@@ -1,1 +1,14 @@
-document.write('<h2>teste...</h2>');
+//document.write('<h2>teste...</h2>');
+
+var elUserneme = document.getElementById('username');
+elUserneme.addEventListener('blur', checkLength, 'false');
+
+
+function checkLength(){
+	var elMsg = document.getElementById('feedback');
+	if(this.value.length < 5) {
+		elMsg.innerHTML = 'O campo deve conter pelo menos 5 caracteres';
+	} else {
+		elMsg.innerHTML = '';
+	}
+}
