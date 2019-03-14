41ec69041f6e1b679570a35f73d4a018fa93337e
Jura <jura.webdeveloper@gmail.com>
2019-03-14 09:58:35 -0300

    função setup funcionando

--- c06/js/load.js
+++ c06/js/load.js
@@ -1,1 +1,7 @@
-document.write('<h2>Teste...</h2>');
+//document.write('<h2>Teste...</h2>');
+function setup() {
+	var textInput;
+	textInput = document.getElementById('username');
+	textInput.focus();
+}
+window.addEventListener('focus', setup, 'false');
