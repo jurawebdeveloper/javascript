bf756ec007f30873495bd85bb299044299479624
Jura <jura.webdeveloper@gmail.com>
2019-03-14 10:42:20 -0300

    checkLength() com parametros e funcao tipInput() funcionando

--- c06/js/focus-blur.js
+++ c06/js/focus-blur.js
@@ -1,14 +1,24 @@
 //document.write('<h2>teste...</h2>');
 
 var elUserneme = document.getElementById('username');
-elUserneme.addEventListener('blur', checkLength, 'false');
+elUserneme.addEventListener('blur', function(e){checkLength(e,5);}, 'false');
+
+var elForm = document.getElementsByTagName('form')[0];
+elForm.addEventListener('focus', function(e){tipInput(e);}, 'false');
 
 
-function checkLength(){
+function checkLength(e,minimo){
+	
+	var target = e.target;
 	var elMsg = document.getElementById('feedback');
-	if(this.value.length < 5) {
-		elMsg.innerHTML = 'O campo deve conter pelo menos 5 caracteres';
+	if(target.value.length < minimo) {
+		elMsg.innerHTML = 'O campo deve conter pelo menos ' + minimo + ' caracteres';
 	} else {
 		elMsg.innerHTML = '';
 	}
+}
+
+function tipInput(e) {
+	var target1 = e.target;
+	target1.placeholder = 'digite no mínimo 5 caracteres...';
 }
