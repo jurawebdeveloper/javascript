c3150c754d6d1de1a2fb28ff266f8aa66bb9b0b8
Jura <jura.webdeveloper@gmail.com>
2019-03-14 11:06:48 -0300

    focus blur final

--- c06/js/focus-blur.js
+++ c06/js/focus-blur.js
@@ -1,13 +1,15 @@
 //document.write('<h2>teste...</h2>');
 
+
 var elUserneme = document.getElementById('username');
 elUserneme.addEventListener('blur', function(e){checkLength(e,5);}, 'false');
 
 var elForm = document.getElementsByTagName('form')[0];
-
 elForm.addEventListener('focus', function(e){tipInput(e);}, 'false');
 
 
+//window.addEventListener('focus', setup, 'false');
+/* isto não está funcionando direito: naão consigo mudar para outro input, fica sempre no username *****/
 function checkLength(e,minimo){
 	
 	var target = e.target;
@@ -27,4 +29,10 @@ function tipInput(e) {
 		texto = 'digite no mínimo 6 caracteres com letras e números'
 	}
 	target1.placeholder = texto;
+}
+
+
+function setup() {
+	var focusInput = document.getElementById('username');
+	focusInput.focus();
 }
