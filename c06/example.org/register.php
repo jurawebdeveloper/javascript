646e10489fde7d2c5783e3128e113ed638dafd2c
Jurandir <jura.webdeveloper@gmail.com>
2019-03-17 18:32:00 -0300

    funcao packageHint funcionando

--- c06/example.org/register.php
+++ c06/example.org/register.php
@@ -2,6 +2,8 @@
 	if(isset($_GET['username'])){
 		$username = ($_GET['username']);
 	} else {$username = '';}
+	$package = ($_GET['package'])
 ?>
 
 <h2>Seja bem vindo(a) <?php echo $username ?></h2>
+<h2>Você escolheu o pacote: <?php echo $package ?></h2>
--- c06/js/form.js
+++ c06/js/form.js
@@ -8,11 +8,20 @@ elTermsHint = document.getElementById('termsHint');
 
 
 elForm.addEventListener('submit', checkTerms, 'false');
-
+elSelectPackage.addEventListener('change', packageHint, 'false');
 
 function checkTerms(e){
     if(!elTerms.checked){
         elPackageHint.innerHTML = 'Você deve aceitar os termos.';
         e.preventDefault();
+    }
+}
+
+function packageHint() {
+    var package = this.options[this.selectedIndex].value;
+    if(package == 'monthly') {
+        elPackageHint.innerHTML = 'Economizr $10 escolhendo o plano anual!';
+    } else {
+        elPackageHint.innerHTML = 'Ótima escolha';
     }
 }
