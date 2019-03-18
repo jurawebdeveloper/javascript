214eb966b943aae366a116a48a31a6c5eae83ef4
Jura <jura.webdeveloper@gmail.com>
2019-03-18 10:11:53 -0300

    beforeunload dont work

--- c06/js/html5-events.js
+++ c06/js/html5-events.js
@@ -1,1 +1,11 @@
-document.write('<h2>teste...</h2>')
+document.write('<h2>teste...</h2>');
+
+function setup() {
+	var textInput = document.getElementById('message');
+	textInput.focus();
+	event.preventDefault();
+}
+
+
+window.addEventListener('DOMContentLoaded', setup, 'false');
+window.addEventListener('beforeunload', function(event){return 'Você tem alterações não salvas.';}, 'false');
