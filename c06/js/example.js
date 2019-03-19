3918fc0406d2e6dded0852ce4d614bf5719099c2
Jura <jura.webdeveloper@gmail.com>
2019-03-19 15:32:24 -0300

    recorderControls is working

--- c06/js/example.js
+++ c06/js/example.js
@@ -1,2 +1,32 @@
-document.write('<h2>teste...</h2>');
+//document.write('<h2>teste...</h2>');
 
+var username, noteName, textEntered, target;
+noteName = document.getElementById('noteName');
+noteInput = document.getElementById('noteInput');
+
+
+document.addEventListener('click', function(e) {recorderControls(e);}, false);
+
+function recorderControls(e) {
+	e = window.event;
+	target = e.target;
+	e.preventDefault();
+	switch(target.getAttribute('data-state')) {
+		case 'record':
+		record(target);
+		break;
+		case 'stop':
+		stop(target);
+		break;
+	}
+}
+
+function record(target) {
+	target.setAttribute('data-state', 'stop');
+	target.textContent = 'stop';
+}
+
+function stop(target) {
+	target.setAttribute('data-state', 'record');
+	target.textContent = 'record';
+}
