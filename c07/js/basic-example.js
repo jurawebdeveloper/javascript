0130d070aaae677d28319afb19ab3de59ea87e00
Jura <jura.webdeveloper@gmail.com>
2019-03-20 19:14:25 -0300

    script example is working

--- c07/js/basic-example.js
+++ c07/js/basic-example.js
@@ -1,1 +1,6 @@
-document.write('<h2>Teste...</h2>')
+//document.write('<h2>Teste...</h2>')
+$(':header').addClass('headline');
+$('li:lt(3)').hide().fadeIn(6500);
+$('li').on('click', function() {
+	$(this).remove();
+});
