8221babc17de77851d7d1c689dd76a968c7b8fdf
Jura <jura.webdeveloper@gmail.com>
2019-03-22 11:10:09 -0300

    after add

--- c07/js/adding-content.js
+++ c07/js/adding-content.js
@@ -2,4 +2,6 @@ document.write('<h2>teste</h2>');
 $(function() {
 	$('ul').before('<p class = "notice">Just Adapted</p>');
 	$('li.hot').prepend('+ ');
+	var $newListItem = $('<li><em>gluten-free </em>soy sauce</li>');
+	$('li:last').after($newListItem);
 });
