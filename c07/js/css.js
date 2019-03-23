a4b39a28804ae2a13c0f8ebae9a6bcc84dbdf3eb
jura <jura.webdeveloper@gmail.com>
2019-03-23 20:20:18 -0300

    background-color changed

---
+++ c07/js/css.js
@@ -0,0 +1,8 @@
+document.write('<h2>teste</h2>');
+$(function() {
+	var backgroundColor = $('li').css('background-color');
+	$('ul').append('<p> Cor de fundo: ' + backgroundColor + '</p>');
+	$('li').css({
+		'background-color':'#c5a996'
+	});
+});
