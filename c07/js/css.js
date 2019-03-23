ba165b6a70f90d17028e0990003ac6c19ae977bc
jura <jura.webdeveloper@gmail.com>
2019-03-23 20:23:15 -0300

    script is working

--- c07/js/css.js
+++ c07/js/css.js
@@ -3,6 +3,7 @@ $(function() {
 	var backgroundColor = $('li').css('background-color');
 	$('ul').append('<p> Cor de fundo: ' + backgroundColor + '</p>');
 	$('li').css({
-		'background-color':'#c5a996'
+		'background-color':'#c5a996',
+		'border': '1px solid #fff',
 	});
 });
