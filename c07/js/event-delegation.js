20baafc9754337b261f96760b863b2a38a821511
Jura <jura.webdeveloper@gmail.com>
2019-03-25 10:23:35 -0300

    teste script completo

--- c07/js/event-delegation.js
+++ c07/js/event-delegation.js
@@ -1,4 +1,14 @@
 $(function() {
-	document.write('<h2>Teste...</h2>');
+	//document.write('<h2>Teste...</h2>');
+	var listItem, itemStatus, eventType;
+	$('ul').on(
+		'click mouseover', ':not(#four)', {status: 'important'},
+		function(e){
+			listItem = 'Item: ' + e.target.id + '<br />';
+			itemStatus = 'Status: ' + e.data.status + '<br />';
+			eventType = 'Event: ' + e.type;
+			$('#notes').html(listItem + itemStatus + eventType);
+		}
+	);
 });
 	
