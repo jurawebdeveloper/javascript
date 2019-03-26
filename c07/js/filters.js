20b499144aafe8ac8152a228fb01252d34b83b57
Jura <jura.webdeveloper@gmail.com>
2019-03-26 10:57:13 -0300

    filters html e script

---
+++ c07/js/filters.js
@@ -0,0 +1,7 @@
+$(function() {
+	var $listItems = $('li');
+	$listItems.filter('.hot:last').removeClass('hot');
+	$('li:not(.hot)').addClass('cool');
+	$listItems.has('em').addClass('complete');
+});
+	
