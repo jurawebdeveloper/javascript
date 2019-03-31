6e6ac4b360028fe5453fc8686a2cdd380587c243
jura <jura.webdeveloper@gmail.com>
2019-03-30 21:39:26 -0300

    example teste function hide-fadeIn

--- c07/js/example.js
+++ c07/js/example.js
@@ -1,5 +1,13 @@
 $(function() {
 	//document.write('<h2>Teste...</h2>');
+	var $list, $newItemForm, $newItemButton;
+	var item = '';
+	$lits = $('ul');
+	$newItemForm = $('#newItemForm');
+	$newItemButton = $('#newItemButton');
 	
+	$('li').hide().each(function(index){
+		$(this).delay(450 * index).fadeIn(1600);
+	})
 });
 	
