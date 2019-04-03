ae595e75ba13f6c3df928544684a86c2f3429754
jura <jura.webdeveloper@gmail.com>
2019-04-03 14:35:28 -0300

    XML script complete

--- c08/js/data-xml.js
+++ c08/js/data-xml.js
@@ -1,28 +1,27 @@
 var xhr = new XMLHttpRequest();
 xhr.onload = function() {
 	if(xhr.status ===200) {
-		var response = xhr.responseXML;
-		var events = response.getElementsByTagName('event');
-		
-		for(var i = 0; i < events.length; i++){
-			var container, image, location1, city, newline;
-			container = document.createElement('div');
-			container.className = 'event';
+		var response = xhr.responseXML;                      // Get XML from the server
+		  var events = response.getElementsByTagName('event'); // Find <event> elements
+
+		  for (var i = 0; i < events.length; i++) {            // Loop through them
+			var container, image, location, city, newline;      // Declare variables
+			container = document.createElement('div');          // Create <div> container
+			container.className = 'event';   
 			
-			image = document.createElement('img');
-			image.setAttribute('src',getNodeValue(events[i], 'map'));
-			image.appendChild(document.createTextNode(getNodeValue(events[i], 'map')));
+			image = document.createElement('img');              // Add map image
+			image.setAttribute('src', getNodeValue(events[i], 'map'));
+			image.setAttribute('alt', getNodeValue(events[i], 'location'));
 			container.appendChild(image);
 			
-			location1 = document.createElement('p')
+			location = document.createElement('p');
 			city = document.createElement('b');
 			newline = document.createElement('br');
-			city.appendChild(document.createTextNode(getNodeValue(events[i], 'loaction')));
-			
-			location1.appendChild(newline);
-			location1.insertBefore(city, newline);
-			location1.appendChild(document.createTextNode(getNodeValue(events[i], 'date')));
-			container.appendChild(location1)
+			city.appendChild(document.createTextNode(getNodeValue(events[i], 'location')));
+			location.appendChild(newline);
+			location.insertBefore(city, newline);
+			location.appendChild(document.createTextNode(getNodeValue(events[i], 'date')));
+			container.appendChild(location);
 			
 			document.getElementById('content').appendChild(container);
 		}
