5c86081a2692e74d68768a2d974d4c69a0ee53c6
jura <jura.webdeveloper@gmail.com>
2019-04-04 09:57:52 -0300

    JSONP terste script insert maps

--- c08/js/data-jsonp.js
+++ c08/js/data-jsonp.js
@@ -2,11 +2,11 @@ function showEvents(data) {
 	var newContent = '';
 	for(var i = 0; i < data.events.length; i++) {
 		newContent += '<div class = "event">';
+		newContent += '<img src = "' + data.events[i].map + '"';
+		newContent += ' alt = "' + data.events[i].location + '" />';
 		newContent += 'teste';
 		newContent += 'teste';
-		newContent += 'teste';
-		newContent += 'teste';
-		newContent += 'teste';
+		newContent += '</div>';
 	}
 
 	document.getElementById('content').innerHTML = newContent;
