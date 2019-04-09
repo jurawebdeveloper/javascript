0d83518643b265b9a45fd7379a433d505bee2b32
jura <jura.webdeveloper@gmail.com>
2019-04-09 15:16:32 -0300

    jq post script works

--- c08/js/jq-post.js
+++ c08/js/jq-post.js
@@ -1,2 +1,7 @@
-
-var 
+$('#register').on('submit', function(e) {
+	e.preventDefault();
+	var details = $('#register').serialize();
+	$.post('register.php', details, function(data) {
+		$('#register').html(data);
+	});
+});
