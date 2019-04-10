bc50b1aab2d1919d265a233f810978210e144f4c
jura <jura.webdeveloper@gmail.com>
2019-04-10 13:21:53 -0300

    jq getJSON teste re

--- c08/js/jq-getJSON.js
+++ c08/js/jq-getJSON.js
@@ -1,1 +1,11 @@
 $('#exchangerates').append('<div id="rates">Teste</div><div id="reload"></div>');
+function loadRates() {
+	$.getJSON('data/rates.json').done(function(data){
+	var msg = '';
+	$.each(data, function(key,val){
+		msg += '<div class = "' + key + '">' + key + ': ' + val + '</div>';
+	});
+	$('#rates').html(msg);
+	})
+}
+loadRates();
