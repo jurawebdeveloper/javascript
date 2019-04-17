7ce231edf401e335b766e7031e04fb3ca021da01
jura <jura.webdeveloper@gmail.com>
2019-04-17 11:22:34 -0300

    local-storage e session storage

---
+++ c09/js/local-storage.js
@@ -0,0 +1,16 @@
+
+if (window.localstorage) {
+	var txtUsername = document.getElementById('username');
+	var txtAnswer = document.getElementById('answer');
+	
+	txtUsername.value = localStorage.getItem('username');
+	txtAnswer.value = localStorage.getItem('answer');
+	
+	txtUsername.addEventListener('input', function() {
+		localStorage.setItem('username',txtUsername.value);
+	}, false);
+	
+	txtAnswer.addEventListener('input', function() {
+		localStorage.setItem('username',txtAnswer.value);
+	}, false);
+}
