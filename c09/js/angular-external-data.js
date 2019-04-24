ef6903e231be1cbb5b3cdd473bf8696663069372
jura <jura.webdeveloper@gmail.com>
2019-04-24 15:34:05 -0300

    angular-external script is working

--- c09/js/angular-external-data.js
+++ c09/js/angular-external-data.js
@@ -1,4 +1,8 @@
 function TimetableCtrl($scope, $http) {
 	$scope.title = 'Session Times';
+	
+	$http.get('js/items.json')
+	.success(function(data){$scope.sessions = data.sessions;})
+	.error(function(data){console.log('error')});
 }
     
