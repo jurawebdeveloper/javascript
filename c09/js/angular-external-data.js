870908634466e939e50dde4b6c7783edf51ab5de
jura <jura.webdeveloper@gmail.com>
2019-04-24 15:18:03 -0300

    angular-external data html

--- c09/js/angular-external-data.js
+++ c09/js/angular-external-data.js
@@ -1,8 +1,6 @@
-// NOTE: Because this example loads external data it will not work locally in some browsers
-// You can try it online at http://javascriptbook.com/code/c09 
-// or run it on your own web server
-function TimetableCtrl($scope, $http) {
-  $http.get('js/items.json')
-  .success(function(data) { $scope.sessions = data.sessions; })
-  .error(function(data) { console.log('error'); });
+function BasketCtrl($scope) {
+	$scope.description = 'Single Ticket';
+	$scope.cost = 8;
+	$scope.qty = 1;
 }
+    
