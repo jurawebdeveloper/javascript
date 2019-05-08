1d2326f30e2785d720f51dd62fd14579274944d7
jura <jura.webdeveloper@gmail.com>
2019-05-08 13:38:36 -0300

    console-log teste console.loh on blur

--- c10/js/console-log.js
+++ c10/js/console-log.js
@@ -1,1 +1,8 @@
 console.log('And we\'re off...');
+var $form, width, height, area;
+$form = $('#calculator');
+
+$('form input[type="text"]').on('blur',function(){
+	console.log('You entered', this.value);
+});
+
