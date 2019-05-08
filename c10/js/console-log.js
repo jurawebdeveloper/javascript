dd33217461715309086e302a53eb6d6843355b3d
jura <jura.webdeveloper@gmail.com>
2019-05-08 14:00:10 -0300

    console-log teste script is working

--- c10/js/console-log.js
+++ c10/js/console-log.js
@@ -6,3 +6,18 @@ $('form input[type="text"]').on('blur',function(){
 	console.log('You entered', this.value);
 });
 
+$('#calculator').on('submit', function(e){
+	e.preventDefault();
+	console.log('Clicked submit...');
+	
+	width = $('#width').val();
+	console.log('Width ' + width);
+	
+	height = $('#height').val();
+	console.log('Height ', height);               // Write height to console
+
+	area = width * height;
+	console.log(area);
+	
+	$form.append('<p>' + area + '</p>');
+})
