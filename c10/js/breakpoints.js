b62276da95130e786a9a2c08c538d5839aee69a0
jura <jura.webdeveloper@gmail.com>
2019-05-09 14:53:20 -0300

    breakpoints final

--- c10/js/breakpoints.js
+++ c10/js/breakpoints.js
@@ -1,17 +1,18 @@
 var $form, width, height, area;
 $form = $('#calculator');
-$('form input[type = "text"]').on('blur', function(){
-	console.assert(this.value > 10, 'Usuário inseriu número menor que 10');
-});
+
 
 $('#calculator').on('submit', function(e) { 
   e.preventDefault();
-  console.log('Clicked submit...');
+  console.log('Clicou submit...');
 
   width = $('#width').val();
   height = $('#height').val();
   area = width * height;
   
-  console.assert($.isNumeric(area), 'Usuário inseriu valor não numérico');
+  if (area < 100) {
+    debugger;             
+  }
+  
   $form.append('<p>' + area + '</p>');
 });
