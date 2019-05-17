7dfbb1cfde3c866754e9375ad491b59b449921d3
jura <jura.webdeveloper@gmail.com>
2019-05-17 20:42:07 -0300

    cap11 modal window script works

--- c11/js/modal-init.js
+++ c11/js/modal-init.js
@@ -1,5 +1,5 @@
 (function(){
-	var $content = $('share-options').detach();
+	var $content = $('#share-options').detach();
 	
 	$('#share').on('click', function(){
 		modal.open({content: $content, width: 340, height: 300});
