fb31a9ef91bb7ba293914b76a10297b6e359e32f
jura <jura.webdeveloper@gmail.com>
2019-05-19 16:07:20 -0300

    cap12 for each script worked

--- c12/js/filter-foreach.js
+++ c12/js/filter-foreach.js
@@ -1,3 +1,38 @@
-var people = [
-
-]
+$(function(){
+	var people = [
+		{                                              // Each person is an object
+		  name: 'Casey',                               // It holds name and rate
+		  rate: 60
+		},
+		{
+		  name: 'Camille',
+		  rate: 80
+		},
+		{
+		  name: 'Gordon',
+		  rate: 75
+		},
+		{
+		  name: 'Nigel',
+		  rate: 120
+		}
+	];
+	
+	var results = [];
+	
+	people.forEach(function(person){
+		if (person.rate >= 0 && person.rate <= 200) {
+			results.push(person);
+		}
+	});
+	
+	var $tableBody = $('<tbody></tbody>');
+	for(var i = 0; i < results.length; i++) {
+		var person = results[i];
+		var $row = $('<tr></tr>');
+		$row.append($('<td></td>').text(person.name));
+		$row.append($('<td></td>').text(person.rate));
+		$tableBody.append($row);
+	}
+	$('thead').after($tableBody);  
+});
