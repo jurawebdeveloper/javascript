11f7d6b4cf1e008532290538fe049cc078053d59
jura <jura.webdeveloper@gmail.com>
2019-05-19 13:32:56 -0300

    slider function advance()

--- c11/js/slider.js
+++ c11/js/slider.js
@@ -5,4 +5,29 @@ $('.slider').each(function(){
 	var $buttonArray = [];
 	var $currentIndex = 0;
 	var timeout;
+	
+	function move(newIndex) {
+		
+	}
+	function advance() {
+		clearTimeout(timeout);
+		timeout = setTimeout(function(){
+			if(currentIndex < ($slides.length - 1)) {
+				move(currentIndex + 1);
+			} else {
+				move(0);
+			}
+		}, 4000)
+	}
+	$.each($slides, function(index){
+		var $button = $('<button type="button" class="slide-btn">&bull</button>');
+		if (index === currentIndex){
+			$button.addClass('active');
+		}
+		$button.on('click', function() {
+			move(index);
+		}).appendTo('.slide-buttons');
+		buttonArray.push($button);
+	});
+	advance();
 });
