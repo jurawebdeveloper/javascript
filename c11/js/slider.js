28ea79785473dd4f9b44253de7928db167cee735
jura <jura.webdeveloper@gmail.com>
2019-05-18 22:41:58 -0300

    slider variaveis script

--- c11/js/slider.js
+++ c11/js/slider.js
@@ -1,3 +1,8 @@
 $('.slider').each(function(){
-	
+	var $this = $(this);
+	var $group = $this.find('.slide-group');
+	var $slides = $this.find('.slide');
+	var $buttonArray = [];
+	var $currentIndex = 0;
+	var timeout;
 });
