2548f7441c503e6b5360e0f5d868654405f6b7fa
jura <jura.webdeveloper@gmail.com>
2019-05-20 11:09:43 -0300

    cap12 dynamic filter html pluguin e script início

---
+++ c12/js/dynamic-filter.js
@@ -0,0 +1,33 @@
+(function(){
+	var people = [
+		{                                              // Each person is an object
+		  name: 'Casey',                               // It holds name and rate
+		  rate: 60
+		},
+		{
+		  name: 'Camille',
+		  rate: 80
+		},
+		{
+		  name: 'Gordon',
+		  rate: 75
+		},
+		{
+		  name: 'Nigel',
+		  rate: 120
+		}
+		{
+		  name: 'Joe',
+		  rate: 92
+		}
+		{
+		  name: 'Ann',
+		  rate: 89
+		}
+		{
+		  name: 'Bob',
+		  rate: 95
+		}
+		
+	];
+}());
