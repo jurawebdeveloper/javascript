308074b55580c937f8075b29c50ea3d6664faf60
jura <jura.webdeveloper@gmail.com>
2019-05-20 11:55:26 -0300

    cap12 dynamic filter script correct

--- c12/js/dynamic-filter.js
+++ c12/js/dynamic-filter.js
@@ -67,7 +67,7 @@
 	}
 	
 	function init(){
-		$('3slider').noUiSlider({
+		$('#slider').noUiSlider({
 			range: [0, 150], start: [65, 90], handles: 2, margin: 20, connect: true, serialization: { to: [$min, $max], resolution: 1}
 		}).change(function(){ update($min.val(), $max.val());});
 		makeRows();
