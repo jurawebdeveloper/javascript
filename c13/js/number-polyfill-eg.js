a984fe7be24a1adf579dfad02b42d923865c54f0
jura <jura.webdeveloper@gmail.com>
2019-05-24 12:05:19 -0300

    validacao html

--- c13/js/number-polyfill-eg.js
+++ c13/js/number-polyfill-eg.js
@@ -1,6 +1,11 @@
-(function(){
-	
-}());
+yepnope({
+    test: Modernizr.inputtypes.number,
+    yep: ['js/numPolyfill.js', 'css/number.css'],
+    nope: ['js/numPolyfill.js', 'css/number.css'],
+    complete: function() {
+        console.log('YepNope + Modernizr are done');
+    }
+});
 
 
 
